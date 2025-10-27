a318a67bfd8b0e2551125faf8930c8acc5a6b33b
Lija Jose <itslijajose@gmail.com>
2025-10-27 14:48:05 +0530

    modified

--- infra/scripts/deploy-shared.sh
+++ infra/scripts/deploy-shared.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+set -e
+
+STACK_NAME="shared-artifacts-stack"
+PROFILE="shared-services"
+REGION="us-east-1"
+
+echo "🚀 Deploying Shared Artifacts Stack ($STACK_NAME) in Shared Services Account..."
+
 aws cloudformation deploy \
-  --stack-name shared-artifacts-stack \
-  --template-file infra/templates/shared-artifacts-stack.yaml \
-  --parameter-overrides file://infra/parameters/cicd-params.json \
+  --stack-name $STACK_NAME \
+  --template-file ../templates/shared-artifacts-stack.yaml \
+  --parameter-overrides \
+    SharedAccountId=782496497250 \
+    DevAccountId=068898927889 \
+    Region=$REGION \
+    BucketNamePrefix=shared-artifacts-collectorcard \
   --capabilities CAPABILITY_NAMED_IAM \
-  --profile shared-services
+  --region $REGION \
+  --profile $PROFILE
+
+echo "✅ Shared Artifacts Stack deployed successfully."
+
+aws cloudformation describe-stacks \
+  --stack-name $STACK_NAME \
+  --profile $PROFILE \
+  --query "Stacks[0].Outputs" --output table
